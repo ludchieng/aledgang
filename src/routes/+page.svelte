4996db3be10037ae1001b8742b6b2279a8c95149
Ludwig Chieng <lud.chieng@hotmail.fr>
2023-09-19 23:22:41 +0200

    feat: add leaflet

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,37 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+  import L from 'leaflet';
+  let map: L.Map | null = null;
+
+  function leaflet(container: HTMLDivElement) {
+    map = L.map(container, {
+      preferCanvas: true,
+      attributionControl: false,
+      zoomControl: false,
+    }).setView([48.862, 2.343], 13);
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}{r}.png', {
+      maxZoom: 21,
+      maxNativeZoom: 19,
+    }).addTo(map);
+
+    return {
+      destroy: () => {
+        map && map.remove();
+        map = null;
+      },
+    };
+  }
+</script>
+
+<svelte:window on:resize={() => map && map.invalidateSize()} />
+
+<div class="map" use:leaflet />
+
+<style>
+  @import 'leaflet/dist/leaflet.css';
+
+  .map {
+    height: 100%;
+    width: 100%;
+  }
+</style>
